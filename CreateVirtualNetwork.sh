84ea47ca8689678f65ea4abbf7a7472da6ba7a69
Pender Sessoms <99392512+apsessoms@users.noreply.github.com>
2024-02-14 06:43:50 -0700

    Delete CreateVirtualNetwork.sh

--- CreateVirtualNetwork.sh
+++
@@ -1,2 +0,0 @@
-#Create Virtual Network - Bicep
-# first sign in by using az login command. show which sub you're in by typing az account show. change sub by typing az account set --subscription BLM_dev_1
